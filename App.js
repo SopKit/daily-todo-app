45433b697cba9f49acbdb7d8e8d2a1d8961b83be
sh20raj <shaswatraj3@gmail.com>
2024-07-24 09:28:47 +0530

    chore: Update WebView source to use hosted URL for Daily To-Do App

--- App.js
+++ App.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
-import * as FileSystem from 'expo-file-system';
 
 export default function App() {
-  const localUri = `${FileSystem.documentDirectory}index.html`;
-  
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: localUri }} style={{ flex: 1 }} />
+      <WebView source={{ uri: 'https://sh20raj.github.io/daily-todo-app/' }} style={{ flex: 1 }} />
     </View>
   );
 }
