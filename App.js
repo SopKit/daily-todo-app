a13133161dcaf1f3e4c30fd451605e5c3e8cda92
sh20raj <shaswatraj3@gmail.com>
2024-07-24 09:22:15 +0530

    app files

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as FileSystem from 'expo-file-system';
 
 export default function App() {
+  const localUri = `${FileSystem.documentDirectory}index.html`;
+  
   return (
     <View style={styles.container}>
-      <WebView source={{ uri: 'file:///android_asset/index.html' }} style={{ flex: 1 }} />
+      <WebView source={{ uri: localUri }} style={{ flex: 1 }} />
     </View>
   );
 }
